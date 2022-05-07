f07ffe6c70574489075959ffd3219c37cc847f00
CrocoRyan <ryan_gg@hotmail.com>
2022-05-06 23:43:50 -0500

    fix sequence of normalize and data-splitting

--- normalize.py
+++ normalize.py
@@ -5,20 +5,50 @@ import pandas as pd
 import codecs
 from sklearn import preprocessing
 
-if __name__ == '__main__':
 
-    data = pd.read_csv('./data/data_r.csv')
+    #
+    # data = pd.read_csv('./data/data_r.csv')
+    #
+    # numeric_attrs = ['coordinate_x',
+    #         	        'coordinate_y', 'decoration_condition','deed',
+    #         	        'elevator', 'facility0', 'facility1', 'facility2',
+    #         	        'facility3', 'facility4', 'facility5','level',
+    #         	        'total', 'framework',
+    #         	        'ownership','apt','lift','district',
+    #         	        'rights','scale','bath','room',
+    #         	        'saloon']
+    #
+    # labels=data['price']
+    # tmp=data[numeric_attrs]
+    # right_dummies = pd.get_dummies(data.rights)
+    # right_dummies.columns=['right_0','right_1','right_2']
+    #
+    # data=pd.concat([tmp, right_dummies], axis=1)
+    # for i in numeric_attrs:
+    #     scaler = preprocessing.StandardScaler()
+    #     tmp[i] = scaler.fit_transform(tmp[i].to_numpy().reshape(-1, 1))
+    # labels.to_csv('./data/labels.csv',index=False)
+    # tmp.to_csv('./data/data_n.csv',index=False)
+class Standarizer:
+    def __init__(self):
+        self.train_X = pd.read_csv('./data/train_feat.csv')
+        self.train_y = pd.read_csv('./data/train_label.csv')
+        self.std_scale = preprocessing.StandardScaler().fit(self.train_X)
+        self.std_scale_y = preprocessing.StandardScaler().fit(self.train_y)
+    def transform(self):
+        test_X = pd.read_csv('./data/test_feat.csv')
+        test_y = pd.read_csv('./data/test_label.csv')
+        X_train = self.std_scale.transform(self.train_X)
+        X_test = self.std_scale.transform(test_X)
+        y_train = self.std_scale_y.transform(self.train_y)
+        y_test = self.std_scale_y.transform(test_y)
+        return X_train,y_train,X_test,y_test
+    def inverse_transform(self,labels):
+        return self.std_scale_y.inverse_transform(labels)
+    # X_train.to_csv('./data/train_feat.csv')
+    # X_test.to_csv('./data/test_feat.csv')
+    # y_train.to_csv('./data/train_label.csv')
+    # y_test.to_csv('./data/test_label.csv')
 
-    numeric_attrs = ['average','coordinate_x',
-            	        'coordinate_y', 'decoration_condition','deed',
-            	        'elevator', 'facility0', 'facility1', 'facility2',
-            	        'facility3', 'facility4', 'facility5','level',
-            	        'total', 'framework','house_term',
-            	        'ownership','purpose','apt','lift','district',
-            	        'rights','scale','structure','bath','kitchen', 'room',
-            	        'saloon']
-    for i in numeric_attrs: 
-        scaler = preprocessing.StandardScaler()
-        data[i] = scaler.fit_transform(data[i].as_matrix().reshape(-1, 1))
 
-    data.to_csv('./data/data_n.csv',index=False)
+
--- split.py
+++ split.py
@@ -3,16 +3,47 @@ import pandas as pd
 
 
 #数据集划分，训练集和测试集，大致各占80%和20%
-def split(data):
-	data_len = data['price'].count()
-	sp = int(data_len*0.8)
-	train_data = data[:sp]
-	test_data = data[sp:]
-	return train_data,test_data
+from sklearn.model_selection import train_test_split
+
+
 
 if __name__ == '__main__':
-	data = pd.read_csv('data_n.csv')
-	train_data,test_data = split(data)
-	train_data.to_csv('train_data.csv',index=False)
-	test_data.to_csv('test_data.csv',index=False)
+	# test_data = pd.read_csv('./data/test_data.csv')
+	# test_y=test_data['price']
+	# test_data=test_data.drop('price', axis=1)
+	# test_data=test_data.drop('average', axis=1)
+	# test_data.to_csv('./data/test_feat.csv',index=False)
+	# test_y.to_csv('./data/test_label.csv',index=False)
+	#
+	# train_data = pd.read_csv('./data/train_data.csv')
+	# train_y=train_data['price']
+	# train_data=train_data.drop('price', axis=1)
+	# train_data=train_data.drop('average', axis=1)
+	# train_data.to_csv('./data/train_feat.csv',index=False)
+	# train_y.to_csv('./data/train_label.csv',index=False)
+
+	data = pd.read_csv('./data/data_r.csv')
+	numeric_attrs = ['coordinate_x',
+					 'coordinate_y', 'decoration_condition', 'deed',
+					 'elevator', 'facility0', 'facility1', 'facility2',
+					 'facility3', 'facility4', 'facility5', 'level',
+					 'total', 'framework',
+					 'ownership', 'apt', 'lift', 'district',
+					 'rights', 'scale', 'bath', 'room',
+					 'saloon']
+	y=data['price']
+	tmp = data[numeric_attrs]
+	right_dummies = pd.get_dummies(data.rights)
+	right_dummies.columns = ['right_0', 'right_1', 'right_2']
+
+	X = pd.concat([tmp, right_dummies], axis=1)
+
+	# X = pd.read_csv('./data/data_n.csv')
+	# y=pd.read_csv('./data/labels.csv')
+
+	X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+	X_train.to_csv('./data/train_feat.csv',index=False)
+	X_test.to_csv('./data/test_feat.csv',index=False)
+	y_train.to_csv('./data/train_label.csv',index=False)
+	y_test.to_csv('./data/test_label.csv',index=False)
 
